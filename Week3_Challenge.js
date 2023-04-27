b5d19d67fec31c2e83953a7286d447f5ffaa3a40
Josh Lin <jlin9695@gmail.com>
2023-04-27 18:32:46 -0500

    Completed Challenge 3

--- Week3_Challenge.js
+++ Week3_Challenge.js
@@ -1,1 +1,9 @@
+function FirstReverse(str) { 
+  
+  // code goes here  
+  return str.split("").reverse().join(""); 
 
+}
+   
+// keep this function call here 
+console.log(FirstReverse(readline()));
