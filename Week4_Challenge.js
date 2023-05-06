cf1f716422eb59d5c5af17c4392ca6d1eae7dc98
Josh Lin <jlin9695@gmail.com>
2023-05-06 00:08:33 -0500

    Update Week4_Challenge.js
    Submit solution for challenge.

--- Week4_Challenge.js
+++ Week4_Challenge.js
@@ -1,1 +1,11 @@
+function LongestWord(sen) { 
 
+  // code goes here  
+  var splitString = sen.split(/[!\s?&,.]+/);
+
+  return splitString.sort((a, b) => b.length - a.length)[0]
+
+}
+   
+// keep this function call here 
+console.log(LongestWord(readline()));
