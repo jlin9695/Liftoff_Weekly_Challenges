76793d0a43f37180ee736f1ce88df2e2880cae2b
Josh Lin <jlin9695@gmail.com>
2023-05-21 11:04:15 -0500

    Updated Week5_Challenge.js

--- Week5_Challenge.js
+++ Week5_Challenge.js
@@ -1,1 +1,13 @@
-
+function numAdd(x){
+  if(x<0 || typeof(n)!= "number") {
+    return  "Enter a number between 1 and 1000.";
+  } else{
+    var y = 0;
+    var z = 1;
+    for(var i = 0; i<n; i++){
+      y += z;
+      z += 1;
+    }
+    return y
+  }
+}
