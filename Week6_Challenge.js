3f2559ad271219b2e4fe7dacbb74387ff1a43faf
Josh Lin <jlin9695@gmail.com>
2023-05-26 12:18:14 -0500

    Update Week6_Challenge.js

--- Week6_Challenge.js
+++ Week6_Challenge.js
@@ -1,1 +1,12 @@
+public static String stringFinder(String word) {
+    String result = "";
 
+    for (int i = 0; i < word.length(); i++) {
+        char c = word.charAt(i);
+        if (Character.isLowerCase(c)) {
+            result += c;
+        }
+    }
+
+    return result;
+}
